5a1026db83efdea332c0d25199b40e0a27207efe
Roberto Pacheco <robertoaraujo230@hotmail.com>
2026-08-13 21:08:21 -0300

    feat: register analytics repositories, geolocation service, and MassTransit

--- src/Minify.Application/DependencyInjection.cs
+++ src/Minify.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
+using Minify.Application.UseCases.Analytics.GetAccesses;
+using Minify.Application.UseCases.Analytics.GetSummary;
 using Minify.Application.UseCases.Url.RetrieveOriginalUrl;
 using Minify.Application.UseCases.Url.ShortenUrl;
 
@@ -9,7 +11,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         AddUseCases(services);
-        
+
         return services;
     }
 
@@ -17,5 +19,7 @@ public static class DependencyInjection
     {
         services.AddScoped<IShortenUrlUseCase, ShortenUrlUseCase>();
         services.AddScoped<IRetrieveOriginalUrlUseCase, RetrieveOriginalUrlUseCase>();
+        services.AddScoped<IGetAnalyticsSummaryUseCase, GetAnalyticsSummaryUseCase>();
+        services.AddScoped<IGetAnalyticsAccessesUseCase, GetAnalyticsAccessesUseCase>();
     }
 }
--- src/Minify.Infrastructure/DependencyInjection.cs
+++ src/Minify.Infrastructure/DependencyInjection.cs
@@ -1,15 +1,22 @@
 using HashidsNet;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Minify.Application.Services.Caching;
+using Minify.Application.Services.Geolocation;
 using Minify.Application.Services.ShortCode;
 using Minify.Domain.Repositories;
 using Minify.Domain.Repositories.ShortUrl;
+using Minify.Domain.Repositories.UrlAccessEvent;
 using Minify.Infrastructure.DataAccess;
 using Minify.Infrastructure.DataAccess.Repositories.ShortUrl;
+using Minify.Infrastructure.DataAccess.Repositories.UrlAccessEvent;
 using Minify.Infrastructure.Services.Caching;
+using Minify.Infrastructure.Services.Geolocation;
+using Minify.Infrastructure.Services.Messaging;
 using Minify.Infrastructure.Services.ShortCode;
+using Minify.Messaging.Publishers;
 using StackExchange.Redis;
 
 namespace Minify.Infrastructure;
@@ -23,6 +30,8 @@ public static class DependencyInjection
         AddRepositories(services);
         AddHashids(services, configuration);
         AddShortCodeServices(services);
+        AddGeolocationService(services, configuration);
+        AddMessaging(services, configuration);
 
         return services;
     }
@@ -50,6 +59,8 @@ public static class DependencyInjection
     {
         services.AddScoped<IShortUrlWriteOnlyRepository, ShortUrlWriteOnlyRepository>();
         services.AddScoped<IShortUrlReadOnlyRepository, ShortUrlReadOnlyRepository>();
+        services.AddScoped<IUrlAccessEventWriteOnlyRepository, UrlAccessEventWriteOnlyRepository>();
+        services.AddScoped<IUrlAccessEventReadOnlyRepository, UrlAccessEventReadOnlyRepository>();
     }
 
     private static void AddHashids(IServiceCollection services, IConfiguration configuration)
@@ -70,5 +81,45 @@ public static class DependencyInjection
         services.AddSingleton<IUrlCacheService, UrlCacheService>();
 
         services.AddHostedService<ShortCodeCounterInitializer>();
+    }
+
+    private static void AddGeolocationService(IServiceCollection services, IConfiguration configuration)
+    {
+        var baseUrl = configuration["Geolocation:ApiUrl"] ?? "http://ip-api.com/";
+        var timeoutMs = configuration.GetValue<int>("Geolocation:TimeoutMs", 500);
+
+        services.AddHttpClient<IGeolocationService, GeolocationService>(client =>
+        {
+            client.BaseAddress = new Uri(baseUrl);
+            client.Timeout = TimeSpan.FromMilliseconds(timeoutMs);
+        });
+    }
+
+    private static void AddMessaging(IServiceCollection services, IConfiguration configuration)
+    {
+        var rabbitMqConnectionString = configuration.GetConnectionString("RabbitMQ") ??
+                                       throw new InvalidOperationException("Connection string 'RabbitMQ' is not configured.");
+
+        services.AddMassTransit(x =>
+        {
+            x.AddConsumer<UrlClickedEventConsumer>();
+
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.Host(rabbitMqConnectionString);
+
+                cfg.ReceiveEndpoint("url-clicked", e =>
+                {
+                    e.UseMessageRetry(r => r.Intervals(
+                        TimeSpan.FromSeconds(5),
+                        TimeSpan.FromSeconds(15),
+                        TimeSpan.FromSeconds(45)));
+
+                    e.ConfigureConsumer<UrlClickedEventConsumer>(context);
+                });
+            });
+        });
+
+        services.AddScoped<IEventPublisher, RabbitMqEventPublisher>();
     }
 }
