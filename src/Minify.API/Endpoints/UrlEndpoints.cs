94746b0b0e2c692986fa850687c015834968a0e9
Roberto Pacheco <robertoaraujo230@hotmail.com>
2026-08-13 21:09:02 -0300

    feat: publish UrlClickedEvent on successful redirect

--- src/Minify.API/Endpoints/UrlEndpoints.cs
+++ src/Minify.API/Endpoints/UrlEndpoints.cs
@@ -3,6 +3,8 @@ using Minify.Application.UseCases.Url.ShortenUrl;
 using Minify.Communication.Requests.Url;
 using Minify.Communication.Responses;
 using Minify.Communication.Responses.Url;
+using Minify.Messaging.Events;
+using Minify.Messaging.Publishers;
 
 namespace Minify.API.Endpoints;
 
@@ -11,13 +13,46 @@ public static class UrlEndpoints
     public static IEndpointRouteBuilder MapUrlEndpoints(this IEndpointRouteBuilder app)
     {
         app.MapGet("/{shortenedCode}",
-            async (string shortenedCode, IRetrieveOriginalUrlUseCase useCase, CancellationToken cancellationToken) =>
+            async (string shortenedCode,
+                   HttpContext httpContext,
+                   IRetrieveOriginalUrlUseCase useCase,
+                   IEventPublisher eventPublisher,
+                   ILogger<Program> logger,
+                   CancellationToken cancellationToken) =>
             {
                 var result = await useCase.Execute(shortenedCode, cancellationToken);
 
                 if (!result.IsSuccess)
                     return Results.BadRequest(ResultJson.Failure(result.Errors));
-                
+
+                if (result.Data.Status == RetrieveOriginalUrlStatus.Found)
+                {
+                    var query = httpContext.Request.Query;
+                    var ev = new UrlClickedEvent
+                    {
+                        ShortCode = shortenedCode,
+                        Timestamp = DateTime.UtcNow,
+                        IpAddress = httpContext.Request.Headers["X-Forwarded-For"].FirstOrDefault()
+                                    ?? httpContext.Connection.RemoteIpAddress?.ToString(),
+                        UserAgent = httpContext.Request.Headers.UserAgent.ToString(),
+                        Referer = httpContext.Request.Headers.Referer.ToString() is { Length: > 0 } r ? r : null,
+                        UtmSource = query["utm_source"].ToString() is { Length: > 0 } s ? s : null,
+                        UtmMedium = query["utm_medium"].ToString() is { Length: > 0 } m ? m : null,
+                        UtmCampaign = query["utm_campaign"].ToString() is { Length: > 0 } c ? c : null
+                    };
+
+                    try
+                    {
+                        using var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+                        cts.CancelAfter(TimeSpan.FromMilliseconds(100));
+                        await eventPublisher.PublishAsync(ev, cts.Token);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "Failed to publish UrlClickedEvent for {ShortCode}", shortenedCode);
+                    }
+                }
+
                 return result.Data.Status switch
                 {
                     RetrieveOriginalUrlStatus.Found => Results.Redirect(result.Data.LongUrl, permanent: false),
@@ -30,8 +65,8 @@ public static class UrlEndpoints
             async (RequestShortenUrlJson request, IShortenUrlUseCase useCase, CancellationToken cancellationToken) =>
             {
                 var result = await useCase.Execute(request, cancellationToken);
-                
-                if(!result.IsSuccess)
+
+                if (!result.IsSuccess)
                     return Results.BadRequest(ResultJson.Failure(result.Errors));
 
                 return Results.Created($"/{result.Data.ShortenCode}", result);
